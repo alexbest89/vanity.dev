849c77cabb3f6384cfe00007f66ebccfeac64476
alexbest89 <doronzo.a@gmail.com>
2017-10-15 19:58:23 +0200

    xxx

--- views/header.php
+++ views/header.php
@@ -15,23 +15,24 @@
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="index.php">Vanity Parrucchieri</a>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="?parrucchieri">Parrucchieri <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="?estetica">Estetica</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link disabled" href="#">Disabled</a>
+                <a class="nav-link" href="contatti.php">Contatti</a>
             </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="text" placeholder="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            <input class="form-control mr-sm-2" type="email" name="email" placeholder="email">
+            <input class="form-control mr-sm-2" type="password" name="password" placeholder="password">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="">Login/registrati</button>
         </form>
     </div>
 </nav>
